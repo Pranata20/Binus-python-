69430b1e157b737d9af3cc143784673b741acebb
Pranata20 <pranata20madaharsa@gmail.com>
2026-06-06 15:51:21 +0700

    Add files via upload

---
+++ ganjilgenap.py
@@ -0,0 +1,5 @@
+angka = 1
+if (angka % 2) == 0:
+   print("{0} adalah bilangan genap". format(angka))
+else:
+   print("{0} adalah bilangan ganjil". format(angka))
---
+++ umur.py
@@ -0,0 +1,21 @@
+umur = 100
+if ( ( umur >= 0 ) and ( umur <= 1 ) ):
+    print("Toddler")
+elif ( ( umur >= 2 ) and ( umur <= 3 ) ):
+    print("Toddler")
+elif ( ( umur >= 4 ) and ( umur <= 5 ) ):
+    print("Preschool")
+elif ( ( umur >= 6 ) and ( umur <= 12 ) ):
+    print("Child")
+elif ( ( umur >= 13 ) and ( umur <= 17 ) ): 
+    print("Teenager")
+elif ( ( umur >= 18 ) and ( umur <= 21 ) ):
+    print("Young Adult")
+elif ( ( umur >= 22 ) and ( umur <= 30 ) ):
+    print("Pre-Adult")
+elif ( ( umur >= 31 ) and ( umur <= 50) ):
+    print("Adult")
+elif ( ( umur >= 51 ) and ( umur <= 70) ):
+    print("Pre-Elderly")
+elif ( ( umur >= 71) ):
+    print("Elderly")
